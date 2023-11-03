0606c174d0c94d7ffde9617b5338782f13e07fc8
Jung-kr <jungwy98@gmail.com>
2023-11-03 17:27:53 +0900

    feat : 섹션 7. PRG, ReirectAttributes

--- item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -58,7 +59,7 @@ public class BasicItemController {
     }
 
     /**
-     * @ModelAttribute("item") Item item
+     * V2.@ModelAttribute("item") Item item
      * model.addAttribute("item", item); 자동 추가
      */
 //    @PostMapping("/add")
@@ -71,7 +72,7 @@ public class BasicItemController {
     }
 
     /**
-     * @ModelAttribute name 생략 가능
+     * V3.@ModelAttribute name 생략 가능
      * model.addAttribute(item); 자동 추가, 생략 가능
      * 생략시 model에 저장되는 name은 클래스명 첫글자만 소문자로 등록 Item -> item
      */
@@ -82,13 +83,33 @@ public class BasicItemController {
     }
 
     /**
-     * @ModelAttribute 자체 생략 가능
+     * V4.@ModelAttribute 자체 생략 가능
      * model.addAttribute(item) 자동 추가
      */
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV4(Item item) {
         itemRepository.save(item);
         return "basic/item";
+    }
+
+    /**
+     * V5.PRG - Post/Redirect/Get
+     */
+//    @PostMapping("/add")
+    public String addItemV5(Item item) {
+        itemRepository.save(item);
+        return "redirect:/basic/items/" + item.getId();
+    }
+
+    /**
+     * V6.RedirectAttributes
+     */
+    @PostMapping("/add")
+    public String addItemV6(Item item, RedirectAttributes redirectAttributes) {
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/basic/items/{itemId}";    // => /basic/items/3?status=true
     }
 
     // 상품 수정 폼
@@ -101,10 +122,9 @@ public class BasicItemController {
 
     // 상품 수정
     @PostMapping("/{itemId}/edit")
-    public String editItem(@PathVariable Long itemId, @ModelAttribute Item item, Model model) {
+    public String editItem(@PathVariable Long itemId, @ModelAttribute Item item) {
         itemRepository.update(itemId, item);
-        model.addAttribute("item", item);
-        return "basic/item";
+        return "redirect:/basic/items/{itemId}";
     }
 
 
